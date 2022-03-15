fcf3cac744efbed6a993dc7224a7a8a654fc90fa
Dhevano Gading <dhevanogading@gmail.com>
2022-03-15 11:35:37 +0700

    first commit

---
+++ src/Main.js
@@ -0,0 +1,21 @@
+import React from "react"
+import { Route, Switch } from "react-router-dom";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Gallery from "./pages/Gallery";
+
+class Main extends React.Component {
+    render() {
+        return (
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/gallery" component={Gallery} />
+                <Route path="/about" component={About} />
+                <Route path="/contact" component={Contact} />
+            </Switch>
+        )
+    }
+}
+export default Main;
